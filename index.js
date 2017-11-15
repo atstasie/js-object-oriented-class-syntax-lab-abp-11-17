0cb469dfa02aad9867e124131f72b44856d42b62
Anastassia Troitskaia <atroitskaia@gmail.com>
2017-11-15 18:30:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ sayHi(){
    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
  }
 }
+
 class Ceo extends BoardMember {
   
   hireEmployee(){
